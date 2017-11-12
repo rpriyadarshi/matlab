2ecf1ed767e5cca73dae592d80ff4578b1a7cd44
ROHIT-WIN10\rohit <rohit@rishkan.com>
2017-11-12 15:53:41 -0800

    Basic ADALM1000 + ADXL327 usage

---
+++ AccelTest.m
@@ -0,0 +1,23 @@
+clear;
+clc;
+
+ADIDevices = daq.getDevices;
+ADISession = daq.createSession('adi');
+
+addAnalogInputChannel(ADISession,'smu1','a','Voltage');
+addAnalogInputChannel(ADISession,'smu1','b','Voltage');
+
+v = 0:1:100;
+
+for i=1:length(v)
+    accl = inputSingleScan(ADISession);
+    x(i) = accl(1);
+    y(i) = accl(2);
+end
+
+% Plot the measured data 
+plot(v, x);
+hold on;
+plot(v, y);
+
+clear ADISession

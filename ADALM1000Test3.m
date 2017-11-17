341313ab768358b33f066ff80558bb5e5edded66
ROHIT-WIN10\rohit <rohit@rishkan.com>
2017-11-16 23:23:52 -0800

    Use random angle

--- ADALM1000Test3.m
+++ ADALM1000Test3.m
@@ -16,9 +16,14 @@ t = (0:L-1)*T;                % Time vector
 n = 2^nextpow2(L);
 dim = 2;
 
-v1 = cos(2*pi*50*t);          % First row wave
-v2 = cos(2*pi*150*t);         % Second row wave
-v3 = cos(2*pi*300*t);         % Third row wave
+a = randperm(300,1);
+a1 = a / 3;
+a2 = 2 * a / 3;
+a3 = a;
+
+v1 = cos(2*pi*a1*t);         % First row wave
+v2 = cos(2*pi*a2*t);         % Second row wave
+v3 = cos(2*pi*a3*t);         % Third row wave
 v4 = v1 + v2 + v3;
 
 % Shift the values to positive.
@@ -34,11 +39,11 @@ P1 = P2(:,1:n/2+1);
 P1(:,2:end-1) = 2*P1(:,2:end-1);
 
 subplot(2,1,1)
-plot(t(1:100),X(1,1:100))
+plot(t(1:100), X(1,1:100))
 title(['Row ',num2str(1),' in the Time Domain (Acquired)'])
 
 subplot(2,1,2)
-plot(0:(Fs/n):(Fs/2-Fs/n),P1(1,1:n/2))
+plot(0:(Fs/n):(Fs/2-Fs/n), P1(1,1:n/2))
 title(['Row ',num2str(1), ' in the Frequency Domain (Acquired)'])
 
 clear s;
